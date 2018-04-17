6d2a2bfd1f3590481dd5d8e1ae1e51396a974bac
nkasprak <nkasprak@cbpp.org>
2018-04-17 16:33:00 -0400

    added census tracts, basic map visible

--- app.js
+++ app.js
@@ -1,15 +1,143 @@
-/*globals require, document, console, window*/
+/*globals require, document, console, window, Promise*/
 (function() {
 "use strict";
+if (typeof(Promise)==="undefined") {
+  Promise = require("promise-polyfill");
+}
 var $ = require("jquery");
 var d3 = require("d3");
 var topojson = require("topojson");
 var Figure = require("./CBPP_Figure")($);
+var GridConfig = require("./gridConfig.json");
+var FileIndex = require("./fileIndex.json");
+var GEOID_tracker = {};
+var geo_data = (function() {
+  var r = {};
+  for (var i = 0, ii = FileIndex.length; i<ii; i++) {
+    r[FileIndex[i]] = {};
+    for (var size in GridConfig) {
+      if (GridConfig.hasOwnProperty(size)) {
+        r[FileIndex[i]][size] = {
+          type:"FeatureCollection",
+          features: []
+        };
+      }
+    }
+  }
+  return r;
+})();
+var URL_BASE;
 require("./app.css");
+var indexes = {};
 var Interactive = function(sel) {
+  new Figure.Figure(sel, {
+    rows: [6/9.4]
+  });
+  $(sel).find(".grid00").empty().addClass("mapwrap");
+  URL_BASE = $("#script_hous4-16-18")[0].src.replace("/js/app.js","");
+  var svg;
+  var projection = d3.geoAlbersUsa(),
+    path = d3.geoPath(projection);
+  getIndexes()
+    .then(getLowResShapes)
+    .then(DrawInitialMap);
+
+  function getLowResShapes() {
+    var requests = [];
+    var PromiseMaker = function(file,x,y) {
+      return new Promise(function(resolve, reject) {
+        $.getJSON(URL_BASE + "/grid/" + file + "/low/" + x + "_" + y + ".json", function(d) {
+          var geo = topojson.feature(d, d.objects.districts);
+          if (typeof(geo_data[file])==="undefined") {
+            geo_data[file] = {};
+          }
+          for (var i = 0, ii = geo.features.length; i<ii; i++) {
+            var GEOID = geo.features[i].properties.GEOID || geo.features[i].properties.GEOID10;
+            if (typeof(GEOID_tracker[GEOID])==="undefined") {
+              geo_data[file].low.features.push(geo.features[i]);
+            }
+            GEOID_tracker[GEOID] = true;
+          }
+          resolve();
+        });
+      });
+    };
+    for (var file in indexes) {
+      if (indexes.hasOwnProperty(file)) {
+        for (var i = 0, ii = indexes[file].low.length; i<ii; i++) {
+          var d = indexes[file].low[i];
+          requests.push(PromiseMaker(file, d[0], d[1]));
+        }
+      }
+    }
+    return Promise.all(requests);
+  }
+
+  function DrawInitialMap() {
+    svg = d3.select(sel + " .mapwrap").append("svg")
+      .attr("viewBox","0 0 940 600")
+      .attr("preserveAspectRatio","xMinYMin");
+    svg.selectAll("g")
+      .data((function(g) {
+        var r = [];
+        for (var size in g) {
+          if (g.hasOwnProperty(size)) {
+            r.push(size);
+          }
+        }
+        return r;
+      })(GridConfig))
+      .enter()
+      .append("g")
+      .attr("class", function(d) {return "size " + d;});
+    svg.selectAll("g.size").selectAll("g")
+      .data(FileIndex)
+      .enter()
+      .append("g")
+      .attr("class", function(d) {
+        return "layer " + d;
+      });
+
+    svg.selectAll("g.size.low").selectAll("g.layer").each(function(layer) {
+      d3.select(this).selectAll("path")
+        .data(geo_data[layer].low.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("stroke-width",0.1)
+        .attr("stroke","#000")
+        .attr("fill","none");
+    });
+  }
 
 };
+
+function getIndexes() {
+  var PromiseMaker = function(folder, gridSize) {
+    var file = URL_BASE + "/grid/" + folder + "/" + gridSize + "/index.json";
+    return new Promise(function(resolve, reject) {
+      $.getJSON(file, function(d) {
+        if (typeof(indexes[folder])==="undefined") {
+          indexes[folder] = {};
+        }
+        indexes[folder][gridSize] = d;
+        resolve();
+      });
+    });
+  };
+  var requests = [];
+  for (var i = 0, ii = FileIndex.length; i<ii; i++) {
+    var folder = FileIndex[i];
+    for (var gridSize in GridConfig) {
+      if (GridConfig.hasOwnProperty(gridSize)) {
+        requests.push(PromiseMaker(folder, gridSize));
+      }
+    }
+  }
+  return Promise.all(requests);
+}
+
 $(document).ready(function() {
-  return new Interactive("#hous-4-16-18");
+  return new Interactive("#hous4-16-18");
 });
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,42 @@ var unzip = require("gulp-unzip");
 var ogr2ogr = require("ogr2ogr");
 var topojson = require("topojson");
 var geojson_bbox = require("geojson-bbox");
+var fips = require("./fips.json");
+function zeroPad(fips) {
+  fips = fips + "";
+  while (fips.length < 2) {
+    fips = "0" + fips;
+  }
+  return fips;
+}
 gl.gulp.task("cbpp_shared_lib", function(cb) {
   gl.get_cbpp_shared_libs(["CBPP_Figure"], cb);
 });
 gl.gulp.task("download_shapefiles", function(cb) {
-  var folder = "https://www2.census.gov/geo/tiger/TIGER2015/";
   var files = [
-    "CBSA/tl_2015_us_cbsa.zip",
-    "CNECTA/tl_2015_us_cnecta.zip",
-    "CSA/tl_2015_us_csa.zip",
-    "METDIV/tl_2015_us_metdiv.zip",
-    "NECTA/tl_2015_us_necta.zip",
-    "NECTADIV/tl_2015_us_nectadiv.zip"
+    "https://www2.census.gov/geo/tiger/TIGER2015/CBSA/tl_2015_us_cbsa.zip",
+    "https://www2.census.gov/geo/tiger/TIGER2015/CNECTA/tl_2015_us_cnecta.zip",
+    "https://www2.census.gov/geo/tiger/TIGER2015/CSA/tl_2015_us_csa.zip",
+    "https://www2.census.gov/geo/tiger/TIGER2015/METDIV/tl_2015_us_metdiv.zip",
+    "https://www2.census.gov/geo/tiger/TIGER2015/NECTA/tl_2015_us_necta.zip",
+    "https://www2.census.gov/geo/tiger/TIGER2015/NECTADIV/tl_2015_us_nectadiv.zip"
   ];
+  for (var state in fips) {
+    if (fips.hasOwnProperty(state)) {
+      files.push("https://www2.census.gov/geo/tiger/TIGER2010/TRACT/2010/tl_2010_" + zeroPad(state) + "_tract10.zip");
+    }
+  }
   var requests = [];
   gl.makeDirectory("./download", function() {
     files.forEach(function(f) {
       requests.push(new Promise(function(resolve, reject) {
         try {
-          if (fs.existsSync("./download/" + f.split("/")[1])) {
+          var filename = f.split("/")[f.split("/").length - 1];
+          if (fs.existsSync("./download/" + filename)) {
             resolve();
           } else {
-            var ws = fs.createWriteStream("./download/" +  f.split("/")[1]);
-            request(folder + f)
+            var ws = fs.createWriteStream("./download/" +  filename);
+            request(f)
               .pipe(ws);
             ws.on("finish", function() {
               console.log("finished downloading " + f);
@@ -49,26 +62,33 @@ gl.gulp.task("unzip_shapefiles", ["download_shapefiles"], function(cb) {
   gl.makeDirectory("./shp", function() {
     var zips = fs.readdirSync("./download");
     var requests = [];
+    var index = [];
     zips.forEach(function(f) {
       var name = f.split(".")[0];
-      gl.makeDirectory("./shp/" + name, function() {
-        requests.push(new Promise(function(resolve, reject) {
+      index.push(name);
+      requests.push(new Promise(function(resolve, reject) {
+        gl.makeDirectory("./shp/" + name, function() {
           try {
             if (fs.readdirSync("./shp/" + name ).length > 0) {
               resolve();
             } else {
               gulp.src("./download/" + f)
                 .pipe(unzip())
+                .on("error", reject)
                 .pipe(gulp.dest("./shp/" + name))
-                .on("finish", resolve);
+                .on("finish", function() {
+                  console.log("finished unzipping " + name);
+                  resolve();
+                });
             }
           } catch (ex) {
             console.log(ex);
             reject(ex);
           }
-        }));
-      });
+        });
+      }));
     });
+    fs.writeFileSync("./fileIndex.json", JSON.stringify(index));
     Promise.all(requests).then(function() {
       cb();
     });
@@ -89,6 +109,7 @@ gl.gulp.task("ogr2ogr", ["geojson_dir","unzip_shapefiles"], function(cb) {
             .stream();
           geojson.pipe(fs.createWriteStream('./geojson/' + f + ".json"))
             .on("finish", function() {
+              console.log("finished ogr2ogr for " + f);
               resolve();
           });
         }
@@ -99,6 +120,7 @@ gl.gulp.task("ogr2ogr", ["geojson_dir","unzip_shapefiles"], function(cb) {
     }));
   });
   Promise.all(requests).then(function() {
+    console.log("finished ogr2ogr for all");
     cb();
   });
 });
@@ -118,30 +140,20 @@ gl.gulp.task("geojson_dir", function(cb) {
 gl.gulp.task("topojson_dir", function(cb) {
   makeDirectory("./topojson", cb);
 });
+var gridConfig = require("./gridConfig.json");
 gl.gulp.task("topojson", ["topojson_dir","ogr2ogr","buildDirectory"], function(done) {
   var files = fs.readdirSync("./geojson");
   var sizes = [];
-  sizes.push(new Promise(function(resolve) {
-    topojson_out({
-      quantize:4000,
-      simplify:0.01,
-      dest:"./topojson/low/"
-    }, resolve);
-  }));
-  sizes.push(new Promise(function(resolve) {
-    topojson_out({
-      quantize:40000,
-      simplify:0.003,
-      dest:"./topojson/medium/"
-    }, resolve);
-  }));
-  sizes.push(new Promise(function(resolve) {
-    topojson_out({
-      quantize:400000,
-      simplify:0.0001,
-      dest:"./topojson/high/"
-    }, resolve);
-  }));
+  var promiseMaker = function(config) {
+    return new Promise(function(resolve) {
+      topojson_out(config, resolve);
+    });
+  };
+  for (var config in gridConfig) {
+    if (gridConfig.hasOwnProperty(config)) {
+      sizes.push(promiseMaker(gridConfig[config]));
+    }
+  }
   Promise.all(sizes).then(function() {
     done();
   });
@@ -157,10 +169,20 @@ gl.gulp.task("topojson", ["topojson_dir","ogr2ogr","buildDirectory"], function(d
               encoding: "utf-8"
             }));
             var topo = topojson.topology({districts:geo});
-            topo = topojson.quantize(topo, settings.quantize);
+            for (var i = 0, ii = topo.objects.districts.geometries.length; i<ii; i++) {
+              var obj = topo.objects.districts.geometries[i].properties;
+              for (var prop in obj) {
+                if (obj.hasOwnProperty(prop)) {
+                  if (prop.indexOf("GEOID")===-1 && prop.indexOf("NAME")===-1) {
+                    delete (obj[prop]);
+                  }
+                }
+              }
+            }
             topo = topojson.presimplify(topo);
             topo = topojson.simplify(topo, settings.simplify);
-            fs.writeFileSync(settings.dest + f, JSON.stringify(topo));
+            fs.writeFileSync(settings.dest + f, JSON.stringify(topo/*, null, " "*/));
+            console.log("wrote " + settings.dest + f);
             resolve();
           }
         });
@@ -171,39 +193,138 @@ gl.gulp.task("topojson", ["topojson_dir","ogr2ogr","buildDirectory"], function(d
     });
   }
 });
-
-gl.gulp.task("topojson_grid", ["topojson"], function() {
-  var gridSize = {
-    "high":0.2,
-    "medium":2,
-    "low":20
-  };
-  gl.makeDirectory("./grid", function() {
-    var sizes = fs.readdirSync("./topojson");
-    sizes.forEach(function(f) {
-      if (typeof(gridSize[f])==="undefined") {
-        console.log("no grid size defined for " + f + ", ignoring");
-      } else {
-        topoGrids(gridSize[f], f);
-      }
+gl.gulp.task("copyTopo", ["topojson_grid"], function(cb) {
+  gulp.src(['grid/**/*'])
+    .pipe(gulp.dest('build/grid'))
+    .on("finish", function() {
+      console.log("done");
+      cb();
     });
-  });
-  function topoGrids(tileSize, file) {
-    var files = fs.readdirSync("./topojson/" + file);
-    files.forEach(function(f) {
-      gl.makeDirectory("./grid/" + f.split(".")[0], function() {
-        topoGrid(tileSize, file, f);
+});
+gl.gulp.task("topojson_grid", ["topojson"], function(cb) {
+  if (fs.existsSync("./grid")) {
+    cb();
+  } else {
+    gl.makeDirectory("./grid", function() {
+      var sizes = fs.readdirSync("./topojson");
+      var tasks = [];
+      sizes.forEach(function(f) {
+        tasks.push(new Promise(function(resolve) {
+          if (typeof(gridConfig[f])==="undefined") {
+            console.log("no grid size defined for " + f + ", ignoring");
+            resolve();
+          } else {
+            topoGrids(gridConfig[f].gridSize, f, resolve);
+          }
+        }));
+      });
+      Promise.all(tasks).then(function() {
+        console.log("finished grid");
+        cb();
       });
     });
   }
-  function topoGrid(tileSize, dir, file) {
+  function topoGrids(tileSize, file, cb) {
+    var files = fs.readdirSync("./topojson/" + file);
+    var tasks = [];
+    files.forEach(function(f) {
+      tasks.push(new Promise(function(resolve) {
+        gl.makeDirectory("./grid/" + f.split(".")[0], function() {
+          topoGrid(tileSize, file, f, resolve);
+        });
+      }));
+    });
+    Promise.all(tasks).then(function() {
+      console.log("finished " + file);
+      cb();
+    });
+  }
+  function topoGrid(tileSize, dir, file, cb) {
     var topo = JSON.parse(fs.readFileSync("./topojson/" + dir + "/" + file, {
       encoding: "utf-8"
     }));
     var geo = topojson.feature(topo, topo.objects.districts);
+    var index = {};
+    var obj = {};
+    var g_xmin, g_ymin, g_xmax, g_ymax;
     geo.features.forEach(function(f) {
       var bbox = geojson_bbox(f);
-      console.log(bbox);
+      var xmin = Math.floor(bbox[0]/tileSize)*tileSize;
+      var xmax = Math.ceil(bbox[2]/tileSize)*tileSize;
+      var ymin = Math.floor(bbox[1]/tileSize)*tileSize;
+      var ymax = Math.ceil(bbox[3]/tileSize)*tileSize;
+      if (typeof(g_xmin)==="undefined") {
+        g_xmin = xmin;
+      }
+      if (typeof(g_xmax)==="undefined") {
+        g_xmax = xmax;
+      }
+      if (typeof(g_ymin)==="undefined") {
+        g_ymin = ymin;
+      }
+      if (typeof(g_ymax)==="undefined") {
+        g_ymax = ymax;
+      }
+      g_xmin = Math.min(xmin, g_xmin);
+      g_xmax = Math.max(xmax, g_xmax);
+      g_ymin = Math.min(ymin, g_ymin);
+      g_ymax = Math.max(ymax, g_ymax);
+      for (var x = xmin; x<=xmax; x+= tileSize) {
+        x = Math.round(x*1000)/1000;
+        if (typeof(obj[x])==="undefined") {
+          obj[x] = {};
+        }
+        for (var y = ymin; y<=ymax; y+= tileSize) {
+          y = Math.round(y*1000)/1000;
+          if (typeof(obj[x][y])==="undefined") {
+            obj[x][y] = {
+              type: "FeatureCollection",
+              features: []
+            };
+          }
+          if (typeof(index[x])==="undefined") {
+            index[x] = {};
+          }
+          index[x][y] = 1;
+          obj[x][y].features.push(f);
+        }
+      }
+    });
+    var settings = gridConfig[dir];
+    function indexFlatten(index) {
+      var r = [];
+      for (var x in index) {
+        if (index.hasOwnProperty(x)) {
+          for (var y in index[x]) {
+            if (index[x].hasOwnProperty(y)) {
+              r.push([x,y]);
+            }
+          }
+        }
+      }
+      return r;
+    }
+    gl.makeDirectory("./grid/" + file.split(".")[0] + "/" + dir, function() {
+      fs.writeFileSync("./grid/" + file.split(".")[0] + "/" + dir + "/index.json",
+        JSON.stringify(indexFlatten(index)));
+      var topoTile;
+      for (var x in obj) {
+        if (obj.hasOwnProperty(x)) {
+          for (var y in obj[x]) {
+            if (obj[x].hasOwnProperty(y)) {
+              topoTile = topojson.topology({districts:obj[x][y]});
+              topoTile = topojson.quantize(topoTile, settings.quantize);
+              //topoTile = topojson.presimplify(topoTile);
+              //topoTile = topojson.simplify(topoTile, settings.simplify);
+              fs.writeFileSync("./grid/" + file.split(".")[0] + "/" + dir + "/" +
+                x.replace(".","p") + "_" +
+                y.replace(".","p") + ".json", JSON.stringify(topoTile));
+            }
+          }
+        }
+      }
+      console.log("finished " + dir + " " + file);
+      cb();
     });
   }
 });
