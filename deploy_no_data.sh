535c41302ec2c514b876fb9b265df77223b1461d
Nick Kasprak <nkasprak@gmail.com>
2024-11-12 17:24:36 -0500

    fix deploy script

--- deploy_no_data.sh
+++ deploy_no_data.sh
@@ -1,10 +1,10 @@
 rsync -av \
   -e "/usr/bin/ssh" \
   --exclude "get_image_local.php" \
-  ./webroot/ cbppapps@apps.cbpp.org:/home/cbppapps/apps.cbpp.org/4-16-18hous_rev6-30-23/
+  ./webroot/ cbppapps@vps42437.dreamhostps.com:/home/cbppapps/apps.cbpp.org/4-16-18hous_rev6-30-23/
 
 rsync -av \
   -e "/usr/bin/ssh" \
   --exclude webroot/data \
   --exclude webroot/topojson \
-  ./node/prod/ cbppapps@apps.cbpp.org:/home/cbppapps/apps.cbpp.org/4-16-18hous_rev6-30-23/js/
+  ./node/prod/ cbppapps@vps42437.dreamhostps.com:/home/cbppapps/apps.cbpp.org/4-16-18hous_rev6-30-23/js/
